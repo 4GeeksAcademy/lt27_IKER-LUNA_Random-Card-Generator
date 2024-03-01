e1bda45bb613a32f7a3e82a429a74f43c5f4b3b8
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-03-01 16:51:28 +0000

    agregar archivos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const card = {
+  simbolos: ["♦", "♥", "♠", "♣"],
+  cardValue: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+  colorSimbolos: ["red", "black"]
 };
+
+function newCard() {
+  let seleccionarSimbolo =
+    card.simbolos[Math.floor(Math.random() * card.simbolos.length)];
+  let seleccionarValor =
+    card.cardValue[Math.floor(Math.random() * card.cardValue.length)];
+  let seleccionarColor =
+    card.colorSimbolos[Math.floor(Math.random() * card.colorSimbolos.length)];
+
+  let elementos = document.getElementsByClassName("simbolo");
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].innerText = seleccionarSimbolo;
+  }
+  let colores = document.getElementsByClassName("simbolo");
+  for (let i = 0; i < elementos.length; i++) {
+    colores[i].style.color = seleccionarColor;
+  }
+
+  document.getElementById("cardValue").innerText = seleccionarValor;
+  /*   document.getElementsByClassName("simbolo").style.color = seleccionarColor;
+   */
+  console.log(seleccionarSimbolo);
+  console.log(seleccionarValor);
+}
+newCard();
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("Button1").addEventListener("click", newCard);
+});
