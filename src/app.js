b35a1dc39f57958980b01c6f49b418c042a06f4b
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-03-02 03:47:25 +0000

    agregue los botones

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-/* eslint-disable */
+// Generador de cartas
+
 import "bootstrap";
 import "./style.css";
 
@@ -19,16 +20,19 @@ function newCard() {
   let seleccionarColor =
     card.colorSimbolos[Math.floor(Math.random() * card.colorSimbolos.length)];
 
-  let elementos = document.getElementsByClassName("simbolo");
+  let elementos = document.getElementsByClassName("fs-1");
   for (let i = 0; i < elementos.length; i++) {
     elementos[i].innerText = seleccionarSimbolo;
   }
-  let colores = document.getElementsByClassName("simbolo");
+  let colores = document.getElementsByClassName("fs-1");
   for (let i = 0; i < elementos.length; i++) {
     colores[i].style.color = seleccionarColor;
   }
 
-  document.getElementById("cardValue").innerText = seleccionarValor;
+  document.getElementById("cardValue").innerHTML =
+    "<div class='position-absolute top-50 start-50 translate-middle'><h1 class='display-1'>" +
+    seleccionarValor +
+    "</h1></div>";
   /*   document.getElementsByClassName("simbolo").style.color = seleccionarColor;
    */
   console.log(seleccionarSimbolo);
@@ -38,4 +42,55 @@ newCard();
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("Button1").addEventListener("click", newCard);
+});
+
+/* Cronometro;
+ */
+
+let segundos = 10;
+let minutos = 0;
+
+function actualizaContador() {
+  if (segundos > 0) {
+    segundos--;
+  } else {
+    if (minutos > 0) {
+      segundos = 59;
+      minutos--;
+    } else {
+      clearInterval(cronometro);
+      newCard();
+      // Reinicia el temporizador
+      minutos = 0;
+      segundos = 10;
+      cronometro = setInterval(actualizaContador, 1000);
+    }
+  }
+
+  let segundosTexto = segundos < 10 ? "0" + segundos : segundos;
+  let minutosTexto = minutos < 10 ? "0" + minutos : minutos;
+
+  document.querySelector("#segundos").innerText = segundosTexto;
+  document.querySelector("#minutos").innerText = minutosTexto;
+}
+
+let cronometro = setInterval(actualizaContador, 1000);
+
+function cambiarDimensiones() {
+  let ancho = document.getElementById("ancho").value;
+  let alto = document.getElementById("alto").value;
+  console.log(ancho);
+  console.log(alto);
+
+  let elementos = document.getElementsByClassName("paraCambiar");
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].style.height = `${alto}px`;
+    elementos[i].style.width = `${ancho}px`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document
+    .getElementById("Button2")
+    .addEventListener("click", cambiarDimensiones);
 });
